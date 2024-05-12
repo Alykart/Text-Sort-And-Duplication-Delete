3273615dfb0cefb6caa87e3c4a8af37c56425090
Mantis <LordUlgrem2@gmail.com>
2024-05-12 03:20:58 +0300

    Update FileUtilityGUI.py

--- FileUtilityGUI.py
+++ FileUtilityGUI.py
@@ -36,12 +36,14 @@ def sort_lines(lines):
 def remove_duplicates(lines):
     return list(dict.fromkeys(lines))
 
-def sort_and_remove_duplicates(input_filename, log_text):
-    sorted_filename = input_filename.split('.')[0] + '_sorted.txt'
-    removed_duplicates_filename = input_filename.split('.')[0] + '_removed_duplicates.txt'
-    process_file(input_filename, sorted_filename, sort_lines, log_text)
-    process_file(sorted_filename, removed_duplicates_filename, remove_duplicates, log_text)
-
+def sort_and_remove_duplicates(log_text):
+    input_filename = filedialog.askopenfilename(title="Выберите файл для сортировки и удаления дубликатов")
+    if input_filename:
+        sorted_filename = input_filename.split('.')[0] + '_sorted.txt'
+        removed_duplicates_filename = input_filename.split('.')[0] + '_removed_duplicates.txt'
+        process_file(input_filename, sorted_filename, sort_lines, log_text)
+        process_file(sorted_filename, removed_duplicates_filename, remove_duplicates, log_text)
+        
 def display_operation(operation_name, processing_function, log_text):
     input_filename = filedialog.askopenfilename(title=f"Выберите файл для {operation_name}")
     if input_filename:
@@ -61,17 +63,12 @@ log_text.pack(fill=tk.BOTH, expand=True)
 # Кнопки в ряд
 button_frame = tk.Frame(root)
 button_frame.pack(pady=10)
-
 sort_button = tk.Button(button_frame, text="Сортировка", command=lambda: display_operation("Сортировка", sort_lines, log_text))
 sort_button.pack(side=tk.LEFT, padx=5)
-
 remove_duplicates_button = tk.Button(button_frame, text="Удаление дубликатов", command=lambda: display_operation("Удаление дубликатов", remove_duplicates, log_text))
 remove_duplicates_button.pack(side=tk.LEFT, padx=5)
-
 sort_and_remove_duplicates_button = tk.Button(button_frame, text="Сортировка и удаление дубликатов", command=lambda: sort_and_remove_duplicates(log_text))
 sort_and_remove_duplicates_button.pack(side=tk.LEFT, padx=5)
-
 exit_button = tk.Button(root, text="Выход", command=root.destroy)
 exit_button.pack(pady=5)
-
 root.mainloop()
